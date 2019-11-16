f72a3e9cbac7f4d23cb06a4338f23b06b9a205ef
Tapsoba <STAPSOBA001@CSBSJU.EDU>
2019-11-15 22:54:36 -0600

    Search class is done. Steffi

--- app/src/main/java/com/example/carpool/Search.java
+++ app/src/main/java/com/example/carpool/Search.java
@@ -5,11 +5,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.carpool.DataTypes.CarPool;
 import com.example.carpool.DataTypes.DBConnection;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
 
     private String location;
+    private ViewPostings viewPostings;
     public static final DBConnection dbc = new DBConnection();
 
     public Search(String location) {
@@ -22,15 +26,21 @@ public class Search extends AppCompatActivity {
      * @param location the place the user would like to go to
      */
 
-    public void searchCarpool(String location){
-        //List carpools = dbc.getCarpools()
-        //get all the locations from the carpools
-        //if location in locations in Carpool
-          //display all carpools to this location
-        //else
-          //message saying, "there is no carpool to this location"
-        //Toast.makeText(this, "Sorry, there is no carpool to this location :(",
-              // Toast.LENGTH_SHORT).show();
+    public ArrayList<CarPool> searchCarpool(String location){
+        ArrayList<CarPool> carPools = dbc.getCarpools();
+        ArrayList<CarPool> carpoolsAvailable = new ArrayList<>();
 
+        for(CarPool carpool : carPools){
+            if(location.equals(carpool.getLocation())){
+                carpoolsAvailable.add(carpool);
+            }
+        }
+
+        if(carpoolsAvailable.equals(null)){
+            Toast.makeText(this, "Sorry, there is no carpool to this location :(",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        return carpoolsAvailable;
    }
 }
